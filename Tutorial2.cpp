a8d571087d3771d9c08dd0e32820c0bcb69ee026
ShadowDrake21 <dimka670020040@gmail.com>
2025-10-14 22:06:28 +0200

    lesson 2: data types

---
+++ Tutorial2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+  // float annualSalary;
+  // cout << "Please enter your annual salary: ";
+  // cin >> annualSalary;
+  // float monthlySalary = annualSalary / 12;
+  // cout << "My monthly salary is: " << monthlySalary << endl;
+  // cout << "In 10 years you will earn: " << annualSalary * 10 << endl;
+
+  // char character  = 'A';
+
+  // int yearOfBirth = 2004;
+  // char gender = 'm';
+  // bool isOlderThan18 = true;
+  // float averageGrade = 4.5;
+  // double balance = 4543435353534.0;
+  // cout << "Size of int is " << sizeof(int) << " bytes\n";
+  // cout << "int min value is  " << INT_MIN << "\n";
+  // cout << "int max value is " << INT_MAX << "\n";
+
+  // cout << "size of unsigned int is " << sizeof(unsigned int) << " bytes\n";
+  // cout << "uint max value is " << UINT_MAX << "\n";
+
+  int intMax = INT_MAX;
+  cout << "int max is " << intMax << "\n";
+  cout << "int max + 1 is " << intMax + 1 << "\n";
+}
