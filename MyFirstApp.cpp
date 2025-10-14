4ba95411c48fd6f3d375ef2256541540eafd460e
ShadowDrake21 <dimka670020040@gmail.com>
2025-10-14 21:34:53 +0200

    hello world

---
+++ MyFirstApp.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!" << endl;
+  cout << "My name is Dmytro." << endl;
+  cout << "Today is Tuesday.";
+  cout << "Have a nice day!" << endl;
+}
