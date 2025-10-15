3d2df954c133945785eef137efac7962e2a645d1
ShadowDrake21 <dimka670020040@gmail.com>
2025-10-15 21:18:24 +0200

    lesson 6: operators

---
+++ cpp-full-course/operators.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // cout << 5 + 2 << endl;
+  // cout << 5 / 2 << endl;
+  // cout << 5 / 2.0 << endl;
+  // cout << 5 % 2 << endl;
+
+  // int counter = 7;
+  // counter++;
+  // cout << counter << endl;
+  // counter--;
+  // cout << counter << endl;
+
+  // int counter2 = 7;
+  // cout << counter2++ << endl;
+  // cout << ++counter2 << endl;
+
+  // int a = 5, b = 5;
+  // cout << (a >= b);
+
+  // int a = 5, b = 5;
+  // cout << (a == 5 || b == 5) << endl;
+
+  // int a = 5, b = 8;
+  // cout << (a == 5 && b == 5 + 3) << endl;
+
+  // =, +=, -=, *=, /=, %=
+  int x = 5;
+  cout << "Initial x is " << x << endl;
+  x += 3;
+  cout << "x += 3 is " << x << endl;
+  x -= 3;
+  cout << "x -= 3 is " << x << endl;
+  x *= 3;
+  cout << "x *= 3 is " << x << endl;
+  x /= 3;
+  cout << "x /= 3 is " << x << endl;
+  x %= 3;
+  cout << "x %= 3 is " << x << endl;
+}
