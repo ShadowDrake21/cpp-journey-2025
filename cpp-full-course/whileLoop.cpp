2374a224d71514b9a6f7eaeafe37c150544e9a3f
ShadowDrake21 <dimka670020040@gmail.com>
2025-10-16 21:35:43 +0200

    lesson 11: while loop (part 1)

---
+++ cpp-full-course/whileLoop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int counter = 100;
+  while (100 <= counter && counter <= 500)
+  {
+    if (counter % 3 == 0 && counter % 5 == 0)
+    {
+      cout << counter << endl;
+    }
+    counter++;
+  }
+}
